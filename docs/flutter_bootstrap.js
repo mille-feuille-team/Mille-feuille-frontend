8f244e96e6b752d9dfd3be53736f9af6ceb6b48c
tRensn <tren.smrty41cr@outlook.com>
2025-09-02 17:01:41 +0900

    deploy4
    できねぇ.....

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "336266204"
+    serviceWorkerVersion: "1675684225"
   }
 });
